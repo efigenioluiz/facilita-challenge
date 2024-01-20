3f8872453bc016c1e72bb4f6833891f1990cd8df
efigenioluiz <hanzou0012@gmail.com>
2024-01-20 14:30:15 -0300

    feat: add index to start the server

---
+++ backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import customerRouter from './routes/customer';
+
+
+const app = express();
+const port = 5000;
+
+const apiPrefix = '/api';
+
+app.use(bodyParser.json());
+app.use(apiPrefix + '/customer', customerRouter);
+
+app.listen(port,'0.0.0.0', () => {
+  console.log(`🔥 Server is running on port ${port} 🔥`);
+});
