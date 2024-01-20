f66599497761c43ce0aa8acbee4cfd9f439a3248
efigenioluiz <hanzou0012@gmail.com>
2024-01-20 14:35:17 -0300

    feat: add customer controller

---
+++ backend/src/controllers/CustomerController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express';
+import { CustomerService } from '../models/services/CustomerService';
+
+export const customerController = {
+  getAllCustomers: async (_req: Request, res: Response) => {
+    const customers = await CustomerService.getAll();
+    res.json(customers);
+  },
+
+  getCustomerById: async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const cutomer = await CustomerService.getById(Number(id));
+    res.json(cutomer);
+  },
+
+  createCustomer: async (req: Request, res: Response) => {
+    const { name, email, coordinateX, coordinateY } = req.body;
+    console.log(name, email, coordinateX, coordinateY);
+    const newCustomer = await CustomerService.create(name, email, coordinateX, coordinateY);
+    res.json(newCustomer);
+  },
+
+//   updateItem: async (req: Request, res: Response) => {
+//     const { id } = req.params;
+//     const { name, description } = req.body;
+//     const updatedCustomer = await CustomerService.update(Number(id), name, description);
+//     res.json(updatedCustomer);
+//   },
+
+  deleteCustomer: async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const deletedCustomer = await CustomerService.delete(Number(id));
+    res.json(deletedCustomer);
+  },
+};
