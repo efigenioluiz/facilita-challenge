e9307f7b33e56d77244d68e52a010eb062e4d675
efigenioluiz <hanzou0012@gmail.com>
2024-01-21 18:37:16 -0300

    feat: add DLL sql to insert costumers

---
+++ backend/migrations/insert_customers.sql
@@ -0,0 +1,63 @@
+INSERT INTO
+    customers (
+        name, email, phone, coordinate_x, coordinate_y
+    )
+VALUES (
+        'João', 'joao@example.com', '123456789', '12.34', '56.78'
+    );
+
+INSERT INTO
+    customers (
+        name, email, phone, coordinate_x, coordinate_y
+    )
+VALUES (
+        'Maria', 'maria@example.com', '987654321', '23.45', '67.89'
+    );
+
+INSERT INTO
+    customers (
+        name, email, phone, coordinate_x, coordinate_y
+    )
+VALUES (
+        'Carlos', 'carlos@example.com', '111222333', '34.56', '78.90'
+    );
+
+INSERT INTO
+    customers (
+        name, email, phone, coordinate_x, coordinate_y
+    )
+VALUES (
+        'Ana', 'ana@example.com', '444555666', '45.67', '89.01'
+    );
+
+INSERT INTO
+    customers (
+        name, email, phone, coordinate_x, coordinate_y
+    )
+VALUES (
+        'Pedro', 'pedro@example.com', '777888999', '56.78', '90.12'
+    );
+
+INSERT INTO
+    customers (
+        name, email, phone, coordinate_x, coordinate_y
+    )
+VALUES (
+        'Luiza', 'luiza@example.com', '222333444', '67.89', '12.34'
+    );
+
+INSERT INTO
+    customers (
+        name, email, phone, coordinate_x, coordinate_y
+    )
+VALUES (
+        'André', 'andre@example.com', '555666777', '78.90', '23.45'
+    );
+
+INSERT INTO
+    customers (
+        name, email, phone, coordinate_x, coordinate_y
+    )
+VALUES (
+        'Paula', 'paula@example.com', '999000111', '89.01', '34.56'
+    );
