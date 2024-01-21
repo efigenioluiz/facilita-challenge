5fa7446b228f2f04bde9cb222d09640d4dc2217b
efigenioluiz <hanzou0012@gmail.com>
2024-01-21 09:29:42 -0300

    feat: add cors into backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import customerRouter from './routes/customer';
+import cors from 'cors';
 
 const app = express();
 const port = 5000;
 
 const apiPrefix = '/api';
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(apiPrefix + '/customer', customerRouter);
 
